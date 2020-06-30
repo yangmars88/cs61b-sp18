5668ad6d58cfc6810ce8df70b3e11c5c48c2b42d
yangmars88 <yangli.batc@gmail.com>
2020-06-29 23:57:46 -0400

    save new NBody

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,61 +1,87 @@
+/**
+ Actually runs our simulation of an universe specified in one of the data files
+ */
 public class NBody {
 
+    private static String backgroundImage = "./images/starfield.jpg"; // background image
+    private static final String backgroundMusic = "./audio/2001.mid"; // background music
 
-    public static double readRadius(String s){
+    /**
+     Returns a double corresponding to the radius of the universe in given file
+     */
+    public static double readRadius(String s) {
         In in = new In(s);
+
         in.readInt();
         double radius = in.readDouble();
+
         return radius;
     }
 
-    public static double readPlanet(String s){
+    /**
+     Returns an array of Planets corresponding to the planets defined in given file
+     */
+    public static Planet[] readPlanets(String s) {
         In in = new In(s);
+
         int n = in.readInt();
         in.readDouble();
+
         Planet[] allPlanets = new Planet[n];
-        for(int i=0; i<n; i++){
-            double xP=in.readDouble();
-            double yP=in.readDouble();
-            double xV=in.readDouble();
-            double yV=in.readDouble();
-            double m=in.readDouble();
-            String img=in.readString();
-            allPlanets[i]=new Planet(xP,yP,xV,yV, m, img);
+        for (int i = 0; i < n; i++) {
+            double xP = in.readDouble();
+            double yP = in.readDouble();
+            double xV = in.readDouble();
+            double yV = in.readDouble();
+            double m = in.readDouble();
+            String img = in.readString();
+            allPlanets[i] = new Planet(xP, yP, xV, yV, m, img);
         }
+
         return allPlanets;
     }
 
-    public static void main(String[] args){
-        //args is a String array save all commands line input.
-        double T= Double.parseDouble(args[0]);
-        double dt= Double.parseDouble(args[1]);
-        String fileName= args[2];
-        if(args.length>3){
+    public static void main(String[] args) {
+
+        /** Read command line argments */
+        double T = Double.parseDouble(args[0]);
+        double dt = Double.parseDouble(args[1]);
+        String filename = args[2];
+        if (args.length > 3) {
             NBody.backgroundImage = args[3];
         }
 
-        //get radius for fileName input in args[2]
-        double radius =readRadius(fileName);
-        Planet[] allPlanets = readPlanet(fileName);
-        //scale the universe to r
+        /** Read Planets and the universe radius defined in filename */
+        double radius = readRadius(filename);
+        Planet[] allPlanets = readPlanets(filename);
+
+        /** Enables double buffering for smooth animation rendering and prevent it from flickering */
+        StdDraw.enableDoubleBuffering();
+        /** Scale the universe to r */
         StdDraw.setScale(-radius, radius);
-        //clear drawing window
+        /** Clear the drawing window. */
         StdDraw.clear();
 
-        //play the space-audio
+        /** Play the space-audio */
         StdAudio.play(backgroundMusic);
 
-        double t=0.0; //time variable
-        int n=allPlanets.length;
+        /**
+         In this main loop, updating and rendering each Planet's movements with increasing time variable: t
+         */
+        double t = 0.0; // time variable
+        int n = allPlanets.length;
         double[] xForces = new double[n];
         double[] yForces = new double[n];
-        while(t<T){
-            for(int i=0; i<n; i++){
-                double fX=allPlanets[i].calcNetForceExertedByX(allPlanets);
-                double fY=allPlanets[i].calcNetForceExertedByY(allPlanets);
-                xForces[i]=fX;
-                yForces[i]=fY;
+        while (t < T) {
+
+            /** Calculate net forces for each Planet */
+            for (int i = 0; i < n; i++) {
+                double fX = allPlanets[i].calcNetForceExertedByX(allPlanets);
+                double fY = allPlanets[i].calcNetForceExertedByY(allPlanets);
+                xForces[i] = fX;
+                yForces[i] = fY;
             }
+
             /** Update each Planet's members */
             for (int i = 0; i < n; i++) {
                 allPlanets[i].update(dt, xForces[i], yForces[i]);

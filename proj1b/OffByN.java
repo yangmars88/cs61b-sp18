19201a3d4d635dfb438b9c8b147ff46237cb88c9
yangmars88 <yangli.batc@gmail.com>
2020-12-16 00:55:34 -0500

    save change

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,12 +1,16 @@
 public class OffByN implements CharacterComparator {
+
     private int n;
+
     public OffByN(int N) {
         n = N;
     }
+
+    /** Returns true for characters that are off by N */
     @Override
     public boolean equalChars(char x, char y) {
         int diff = x - y;
-        if(Math.abs(diff) == n ) {
+        if (Math.abs(diff) == n) {
             return true;
         }
         return false;
